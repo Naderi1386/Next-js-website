6d8dfbf5d3ce0dc4b7a88a29c9e6a59c98635270
unknown <mh.naderi1386@gmail.com>
2025-04-09 17:27:11 +0330

    add a logo

--- app/_components/Header.tsx
+++ app/_components/Header.tsx
@@ -1,8 +1,13 @@
+import Logo from "./Logo";
 
 const Header = () => {
   return (
-    <div>Header</div>
-  )
+    <header className="flex items-center justify-between gap-8 bg-[#111722] px-18 py-8">
+      <div className="flex items-center gap-4">
+        <Logo />
+      </div>
+    </header>
+  );
 }
 
 export default Header
---
+++ app/_components/Logo.tsx
@@ -0,0 +1,20 @@
+import Image from "next/image";
+import Link from "next/link";
+
+const Logo = () => {
+  return (
+    <div>
+      <Link href={"/"}>
+        <Image
+          src={"https://go.demo.nextjstemplates.com/images/logo/logo-dark.svg"}
+          alt="Logo"
+          width={200}
+          height={180}
+          className="cursor-pointer"
+        />
+      </Link>
+    </div>
+  );
+};
+
+export default Logo;
