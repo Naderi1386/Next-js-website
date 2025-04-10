55c5ef6aa795311704e5b31e4bac364d50922ab5
unknown <mh.naderi1386@gmail.com>
2025-04-10 17:18:08 +0330

    change ui

--- app/_components/Button.tsx
+++ app/_components/Button.tsx
@@ -3,8 +3,20 @@ import { ReactNode } from "react"
 interface ButtonPropsType{
     children:ReactNode
     color:"blue" | "dark"
+    size?:"full" | "default" 
 }
-const Button = ({children,color}: ButtonPropsType) => {
+const Button = ({children,color,size}: ButtonPropsType) => {
+  if(size==="full") return (
+    <button
+      className={`rounded-sm w-full px-5 py-3 text-white transition-all duration-150 cursor-pointer ${
+        color === "blue"
+          ? "bg-[#4a6cf7]  hover:opacity-90 "
+          : "bg-[#222c40] hover:bg-[#4a6cf7]"
+      }`}
+    >
+      {children}
+    </button>
+  );
   return (
     <button
       className={`rounded-sm px-5 py-3 text-white transition-all duration-150 cursor-pointer ${
--- app/_components/MobileMenuSection.tsx
+++ app/_components/MobileMenuSection.tsx
@@ -3,20 +3,32 @@
 import { IoClose } from "react-icons/io5";
 import HeaderSearch from "./HeaderSearch";
 import Logo from "./Logo";
+import Button from "./Button";
 interface MobileMenuSectionPropsType{
     onClose:()=>void
 }
 const MobileMenuSection = ({onClose}: MobileMenuSectionPropsType) => {
   return (
-    <div className="bg-[#111722] fixed inset-0 px-18 py-4 font-semibold z-[100]">
+    <div className="bg-[#111722] fixed inset-0 px-8 py-4 font-semibold z-[100] flex flex-col">
       <div className="flex items-center justify-between">
         <Logo />
-          <div className="pt-2 flex items-center gap-4">
-            <HeaderSearch />
-            <button onClick={onClose} className="cursor-pointer">
-              <IoClose color="white" size={28} />
-            </button>
-          </div>
+        <div className="pt-2 flex items-center gap-4">
+          <HeaderSearch />
+          <button onClick={onClose} className="cursor-pointer">
+            <IoClose color="white" size={34} />
+          </button>
+        </div>
+      </div>
+      <div className="grow">
+        
+      </div>
+      <div className="flex items-center gap-4">
+        <Button size="full" color="blue">
+          Sign in
+        </Button>
+        <Button size="full" color="dark">
+          Sign up
+        </Button>
       </div>
     </div>
   );
