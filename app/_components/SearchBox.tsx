49db7ce655f2f824d8e308bf88aefcf8aa8dbf90
unknown <mh.naderi1386@gmail.com>
2025-04-10 16:23:09 +0330

    fix some ui problems

--- app/_components/SearchBox.tsx
+++ app/_components/SearchBox.tsx
@@ -8,29 +8,28 @@ interface SearchBoxPropsType {
 }
 
 const SearchBox = ({ handleChange }: SearchBoxPropsType) => {
-  const [inputVal, setInputVal] = useState("aaaa");
+  const [inputVal, setInputVal] = useState("");
   return (
-    <div>
+    <div className="flex items-center justify-center fixed inset-0">
       <div
-        className="flex items-center justify-center fixed inset-0  backdrop-brightness-50 backdrop-blur-[2px] text-white"
+        className="  fixed inset-0  backdrop-brightness-50 backdrop-blur-[2px] "
         onClick={() => handleChange(false)}
-      >
-        <div className="w-[600px] h-[550px] bg-[#111722]  rounded-[8px]">
-          <div className="px-4 py-6 border-b border-solid border-white/15 flex items-center gap-3 font-normal">
-            <IoSearch size={26} color="white" className="cursor-pointer" />
-            <input
-              value={inputVal}
-              onChange={(e) => setInputVal(e.target.value)}
-              type="text"
-              placeholder="Search Entire Site | Products, Docs, Pages ..."
-              className="grow outline-none"
-            />
-            {inputVal && (
-              <button className="cursor-pointer" onClick={() => setInputVal("")}>
-                <IoClose size={26} color="white" />
-              </button>
-            )}
-          </div>
+      ></div>
+      <div className="w-[600px] h-[550px] bg-[#111722]  rounded-[8px] text-white z-[10]">
+        <div className="px-4 py-6 border-b border-solid border-white/15 flex items-center gap-3 font-normal">
+          <IoSearch size={26} color="white" className="cursor-pointer" />
+          <input
+            value={inputVal}
+            onChange={(e) => setInputVal(e.target.value)}
+            type="text"
+            placeholder="Search Entire Site | Products, Docs, Pages ..."
+            className="grow outline-none"
+          />
+          {inputVal && (
+            <button className="cursor-pointer" onClick={() => setInputVal("")}>
+              <IoClose size={26} color="white" />
+            </button>
+          )}
         </div>
       </div>
     </div>
