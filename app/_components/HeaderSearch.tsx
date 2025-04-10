44ab76806c312baa5fa215576436073bed748bf5
unknown <mh.naderi1386@gmail.com>
2025-04-10 16:20:18 +0330

    add a searchbox

--- app/_components/HeaderSearch.tsx
+++ app/_components/HeaderSearch.tsx
@@ -1,6 +1,7 @@
 "use client";
 import { useState } from "react";
 import { IoSearch } from "react-icons/io5";
+import SearchBox from "./SearchBox";
 
 const HeaderSearch = () => {
   const [isOpen, setIsOpen] = useState(false);
@@ -9,6 +10,7 @@ const HeaderSearch = () => {
       <button onClick={() => setIsOpen(true)} className="cursor-pointer pt-2">
         <IoSearch color="white" size={26} />
       </button>
+      {isOpen && <SearchBox handleChange={(val:boolean)=>setIsOpen(val)} />}
     </div>
   );
 };
---
+++ app/_components/SearchBox.tsx
@@ -0,0 +1,40 @@
+"use client";
+
+import { useState } from "react";
+import { IoClose, IoSearch } from "react-icons/io5";
+
+interface SearchBoxPropsType {
+  handleChange: (val: boolean) => void;
+}
+
+const SearchBox = ({ handleChange }: SearchBoxPropsType) => {
+  const [inputVal, setInputVal] = useState("aaaa");
+  return (
+    <div>
+      <div
+        className="flex items-center justify-center fixed inset-0  backdrop-brightness-50 backdrop-blur-[2px] text-white"
+        onClick={() => handleChange(false)}
+      >
+        <div className="w-[600px] h-[550px] bg-[#111722]  rounded-[8px]">
+          <div className="px-4 py-6 border-b border-solid border-white/15 flex items-center gap-3 font-normal">
+            <IoSearch size={26} color="white" className="cursor-pointer" />
+            <input
+              value={inputVal}
+              onChange={(e) => setInputVal(e.target.value)}
+              type="text"
+              placeholder="Search Entire Site | Products, Docs, Pages ..."
+              className="grow outline-none"
+            />
+            {inputVal && (
+              <button className="cursor-pointer" onClick={() => setInputVal("")}>
+                <IoClose size={26} color="white" />
+              </button>
+            )}
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default SearchBox;
