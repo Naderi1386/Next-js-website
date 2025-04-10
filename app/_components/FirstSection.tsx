c88d75916eec7315f7bac65a41828ec8f8e469f9
unknown <mh.naderi1386@gmail.com>
2025-04-10 20:15:02 +0330

    add some responsive styles

--- app/_components/FirstSection.tsx
+++ app/_components/FirstSection.tsx
@@ -2,7 +2,7 @@ import FirstSectionAbout from "./FirstSectionAbout";
 
 const FirstSection = () => {
   return (
-    <div className=" bg-gradient-to-r from-[#0c0a09] to-[#1d293d] text-white px-8 sm:px-22 py-28">
+    <div className=" bg-gradient-to-b from-[#1d293d] to-[#0c0a09] lg:bg-gradient-to-r lg:from-[#0c0a09] lg:to-[#1d293d] text-white px-8 sm:px-22 py-28">
       <div className="flex flex-col justify-center lg:flex-row sm:items-center lg:justify-between gap-16">
         <FirstSectionAbout />
         <div className="bg-red-500 basis-[50%]">s</div>
