abb2bbbace3ad447f0073d4f16d3f43a61e1a6ce
unknown <mh.naderi1386@gmail.com>
2025-04-11 15:29:24 +0330

    add a new breakpoint

--- app/_components/FirstSectionImages.tsx
+++ app/_components/FirstSectionImages.tsx
@@ -4,7 +4,7 @@ const FirstSectionImages = () => {
   return (
     <div className="basis-full custome-lg:basis-[50%] flex justify-end">
       <div className="w-full relative">
-        <div className="relative w-[560px] custome-lg:w-full  h-[500px]">
+        <div className="relative w-full sm:w-[560px] custome-lg:w-full  h-[500px]">
           <Image
             fill
             className="object-cover"
@@ -12,8 +12,8 @@ const FirstSectionImages = () => {
             src={"/images/image-2.webp"}
           />
         </div>
-        <div className=" backdrop-blur-sm border border-solid border-white/10 w-[350px] h-[410px]  absolute left-[-.5rem] bottom-[-1.8rem]  "></div>
-        <div className="w-[350px] h-[410px]  absolute left-[-2.2rem] bottom-[-3.5rem] ">
+        <div className=" backdrop-blur-sm border border-solid border-white/10 w-[300px] h-[350px] sm2x:w-[350px] sm2x:h-[410px]   absolute left-[-.2rem] sm2x:left-[-.5rem] bottom-[-1.8rem]  "></div>
+        <div className="w-[300px] h-[350px] sm2x:w-[350px] sm2x:h-[410px]  absolute left-[-1.5rem] sm2x:left-[-2.2rem] bottom-[-3.5rem] ">
           <Image
             alt="img-1"
             fill
