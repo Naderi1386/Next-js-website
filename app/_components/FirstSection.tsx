b25328bca174e65fcb9def00cb621db706f9d609
unknown <mh.naderi1386@gmail.com>
2025-04-11 14:55:49 +0330

    add some new styles

--- app/_components/FirstSection.tsx
+++ app/_components/FirstSection.tsx
@@ -3,7 +3,7 @@ import FirstSectionImages from "./FirstSectionImages";
 
 const FirstSection = () => {
   return (
-    <div className=" bg-gradient-to-b from-[#1d293d] to-[#0c0a09] lg:bg-gradient-to-r lg:from-[#0c0a09] lg:to-[#1d293d] text-white px-8 sm:px-22 py-28">
+    <div className=" bg-gradient-to-b from-[#1d293d] to-[#0c0a09] lg:bg-gradient-to-r lg:from-[#0c0a09] lg:to-[#1d293d] text-white px-8 sm:px-28 py-28">
       <div className="flex flex-col justify-center lg:flex-row sm:items-center lg:justify-between gap-16">
         <FirstSectionAbout />
         <FirstSectionImages />
--- app/_components/FirstSectionImages.tsx
+++ app/_components/FirstSectionImages.tsx
@@ -3,7 +3,7 @@ import Image from "next/image"
 const FirstSectionImages = () => {
   return (
     <div className="basis-full lg:basis-[50%] flex justify-end">
-      <div className="w-[80%] relative">
+      <div className="w-full relative">
         <div className="relative h-[500px]">
           <Image
             fill
@@ -12,8 +12,14 @@ const FirstSectionImages = () => {
             src={"/images/image-2.webp"}
           />
         </div>
-        <div className="w-[350px] h-[410px]  absolute left-[-4rem] bottom-[-2rem] ">
-          <Image alt="img-1" fill className="object-cover" src={"/images/image-1.webp"} />
+        <div className=" backdrop-blur-sm border border-solid border-white/10 w-[350px] h-[410px]  absolute left-[-.5rem] bottom-[-1.8rem]  "></div>
+        <div className="w-[350px] h-[410px]  absolute left-[-2.2rem] bottom-[-3.5rem] ">
+          <Image
+            alt="img-1"
+            fill
+            className="object-cover"
+            src={"/images/image-1.webp"}
+          />
         </div>
       </div>
     </div>
