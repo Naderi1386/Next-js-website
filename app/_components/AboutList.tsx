67854a9dd91014bcb1d1aa155669d4d2653e2e9e
unknown <mh.naderi1386@gmail.com>
2025-04-13 00:16:25 +0330

    add some responsive styles

--- app/_components/AboutList.tsx
+++ app/_components/AboutList.tsx
@@ -34,12 +34,16 @@ interface AboutListPropsType {
   type: string;
 }
 const AboutList = ({ type }: AboutListPropsType) => {
-    
   const item = aboutItems.find((item) => item.type === type);
+
   return (
     <div className="text-white mt-16  px-2 lg:px-16 ">
       <div className="flex flex-col custome-lg:flex-row custome-lg:items-center custome-lg:justify-between gap-10">
-        <div className={`basis-full lg:basis-[50%]  ${type !== "mission" && "order-1"} py-8`}>
+        <div
+          className={`basis-full lg:basis-[50%]  ${
+            type !== "mission" && "order-1"
+          } py-8`}
+        >
           <h2 className="font-bold text-white text-[2.3rem] w-full sm2x:w-[450px] mb-7">
             {item?.title}
           </h2>
@@ -55,7 +59,7 @@ const AboutList = ({ type }: AboutListPropsType) => {
           ))}
         </div>
         <div className="basis-full custome-lg:basis-[50%] relative">
-          <div className="w-[70%] relative h-[480px]">
+          <div className="w-[100%] sm2x:w-[70%] relative h-[480px]">
             <Image
               src={"/images/img-1.webp"}
               alt="img-1"
@@ -63,8 +67,8 @@ const AboutList = ({ type }: AboutListPropsType) => {
               className="object-cover"
             />
           </div>
-          <div className="w-[320px] h-[365px] absolute right-10 bottom-16 backdrop-blur-sm border border-solid border-white/10"></div>
-          <div className="w-[320px] h-[365px] absolute right-3.5 bottom-10">
+          <div className="w-[240px] h-[290px] sm2x:w-[320px] sm2x:h-[365px] absolute right-[-.3rem] bottom-10 sm2x:bottom-16 backdrop-blur-sm border border-solid border-white/10"></div>
+          <div className="w-[240px] h-[290px] sm2x:w-[320px] sm2x:h-[365px] absolute right-[-1.5rem] bottom-5 sm2x:right-3.5 sm2x:bottom-10">
             <Image
               fill
               className="object-cover"
