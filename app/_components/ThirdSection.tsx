306993f8dc67b3e97c356a7f0cce6bf3e4db7002
unknown <mh.naderi1386@gmail.com>
2025-04-12 16:20:38 +0330

    add some new styles

---
+++ app/_components/ThirdSection.tsx
@@ -0,0 +1,23 @@
+import Title from "./Title";
+
+const ThirdSection = () => {
+  return (
+    <section id="about">
+      <div className="px-8 sm:px-18 py-20 bg-[#0c0a09]">
+        <div className="text-center text-white relative">
+          <Title>About</Title>
+          <h2 className="font-bold md:w-[582px] md:mx-auto text-[34px]  md:text-[45px] leading-[58px]">
+            Know Details About Our Company
+          </h2>
+          <p className="mt-7 md:mx-auto md:w-[640px] text-[#79808a]">
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. In
+            convallis tortor eros. Donec vitae tortor lacus. Phasellus aliquam
+            ante in maximus.
+          </p>
+        </div>
+      </div>
+    </section>
+  );
+}
+
+export default ThirdSection
--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,13 @@
 import FirstSection from "./_components/FirstSection";
 import SecondSection from "./_components/SecondSection";
+import ThirdSection from "./_components/ThirdSection";
 
 export default function Home() {
   return (
     <div>
       <FirstSection />
       <SecondSection />
+      <ThirdSection />
     </div>
   );
 }
