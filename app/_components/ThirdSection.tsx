e28105dabb98b6ee3dba85200db803527feba2b8
unknown <mh.naderi1386@gmail.com>
2025-04-13 21:16:09 +0330

    change ui

--- app/_components/ThirdSection.tsx
+++ app/_components/ThirdSection.tsx
@@ -5,7 +5,7 @@ const ThirdSection = () => {
   return (
     <section id="about">
       <div className="px-8 sm:px-18 py-20 bg-[#0c0a09]">
-        <div className="text-center text-white relative">
+        <div className="text-center text-white relative ">
           <Title>About</Title>
           <h2 className="font-bold md:w-[582px] md:mx-auto text-[34px]  md:text-[45px] leading-[58px]">
             Know Details About Our Company
@@ -16,9 +16,9 @@ const ThirdSection = () => {
             ante in maximus.
           </p>
         </div>
-        <div className="mt-24">
-            <About />
-        </div>
+      </div>
+      <div className=" px-8 sm:px-18 pb-20 pt-28 bg-gradient-to-t from-[#0f172b] to-[#0c0a09]">
+        <About />
       </div>
     </section>
   );
