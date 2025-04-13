93d2ac114e74ea5320c4455a45d4e88255b913ab
unknown <mh.naderi1386@gmail.com>
2025-04-13 17:27:26 +0330

    add some new icons

--- app/_components/DevelopersList.tsx
+++ app/_components/DevelopersList.tsx
@@ -1,4 +1,7 @@
+"use client";
 import Image from "next/image";
+import { useState } from "react";
+import DeveloperIcons from "./DeveloperIcons";
 
 const developers = [
   {
@@ -19,10 +22,16 @@ const developers = [
 ];
 
 const DevelopersList = () => {
+  const [Show, setShow] = useState<null | number>(null);
   return (
-    <ul className="flex justify-center items-center gap-7 mt-16">
-      {developers.map((dev) => (
-        <li key={dev.name} className="w-[22%] text-center relative">
+    <ul className="flex justify-center items-center gap-7 mt-18">
+      {developers.map((dev, index) => (
+        <li
+          onMouseEnter={() => setShow(index)}
+          onMouseLeave={() => setShow(null)}
+          key={dev.name}
+          className="w-[22%] text-center "
+        >
           <div className="w-full relative h-[295px] mb-6">
             <Image
               alt={`person-${dev.name}-img`}
@@ -30,10 +39,9 @@ const DevelopersList = () => {
               fill
               className="object-cover rounded-md"
             />
+            {Show===index && <DeveloperIcons />}
           </div>
-          <h3 className="text-white font-bold  text-[22px] mb-2">
-            {dev.name}
-          </h3>
+          <h3 className="text-white font-bold  text-[22px] mb-2">{dev.name}</h3>
           <span className="text-[#79808a] font-semibold">{dev.skill}</span>
         </li>
       ))}
--- app/_components/Title.tsx
+++ app/_components/Title.tsx
@@ -5,7 +5,7 @@ interface TitlePropsType{
 }
 const Title = ({children}: TitlePropsType) => {
   return (
-    <span className="block absolute left-0 right-0 top-[-1.8rem] sm2x:top-[-2rem] lg:top-[-4rem] text-5xl sm2x:text-6xl lg:text-8xl font-bold opacity-[2.5%]">
+    <span className="block absolute left-0 right-0 top-[-1.8rem] sm2x:top-[-2rem] lg:top-[-4rem] text-5xl sm2x:text-6xl lg:text-8xl font-bold opacity-[2.8%]">
       {children}
     </span>
   );
