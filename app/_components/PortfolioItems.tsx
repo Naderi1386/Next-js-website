c3978d4a2ade5a0d52381b76b5ef0755d27bb31b
unknown <mh.naderi1386@gmail.com>
2025-04-14 23:53:55 +0330

    add some new photo

--- app/_components/PortfolioItems.tsx
+++ app/_components/PortfolioItems.tsx
@@ -1,16 +1,23 @@
 "use client";
 
+import Image from "next/image";
 import { useState } from "react";
 
 const portfolioItems = [
-  { type: ["Brand", "Ecommerce"], img: "./images/brand-1.webp" },
-  { type: ["Brand", "Ecommerce"], img: "./images/brand-2.webp" },
-  { type: ["Brand"], img: "./images/brand-3.webp" },
-  { type: ["Ecommerce"], img: "./images/ecommerce-3.webp" },
+  { type: ["Brand", "Ecommerce"], img: "/images/brand-1.webp" },
+  { type: ["Brand", "Ecommerce"], img: "/images/brand-2.webp" },
+  { type: ["Brand"], img: "/images/brand-3.webp" },
+  { type: ["Ecommerce"], img: "/images/ecommerce-3.webp" },
 ];
 
 const PortfolioItems = () => {
   const [filter, setFilter] = useState<"All" | "Brand" | "Ecommerce">("All");
+  const data =
+    filter === "All"
+      ? portfolioItems
+      : portfolioItems.filter((item) => item.type.includes(filter));
+
+  const isAllData = data.length === 4;
   return (
     <div className="mt-20">
       <ul className="flex items-center justify-center gap-5 text-white font-semibold mb-12">
@@ -33,7 +40,40 @@ const PortfolioItems = () => {
           <span>Ecommerce</span>
         </li>
       </ul>
-
+      <div className="flex items-start justify-between gap-24 px-16">
+        <div className=" basis-[70%] flex items-start gap-2">
+          <div className="basis-[55%]">
+            <div className="relative w-full h-40">
+              <Image
+                alt="img-1"
+                src={data.at(0)?.img as string}
+                fill
+                className="object-cover rounded-md"
+              />
+            </div>
+          </div>
+          <div className="basis-[45%]">
+            <div className="relative w-full h-[35rem]">
+              <Image
+                alt="img-2"
+                src={data.at(1)?.img as string}
+                fill
+                className="object-cover rounded-md"
+              />
+            </div>
+          </div>
+        </div>
+        <div className=" basis-[30%]">
+          <div className="relative w-full h-60">
+            <Image
+              alt="img-3"
+              src={data.at(data.length - 1)?.img as string}
+              fill
+              className="object-cover rounded-md"
+            />
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
