17191a0f213fe7fdea40089568da87f4ed32f64a
unknown <mh.naderi1386@gmail.com>
2025-04-15 22:28:40 +0330

    change ui

--- app/_components/PortfolioLink.tsx
+++ app/_components/PortfolioLink.tsx
@@ -9,15 +9,15 @@ const PortfolioLink = () => {
       initial={{ opacity: 0, y: 20 }}
       animate={{ opacity: 1, y: 0 }}
       exit={{ opacity: 0, y: 20 }}
-      className="bg-[#000]/30 opacity-0 absolute left-0 right-0 bottom-4 p-5 flex items-center justify-between gap-4 mx-auto w-[80%] backdrop-blur-[30px]  rounded-sm  text-black"
+      className="bg-[#000]/30 opacity-0 absolute left-0 right-0 bottom-[6px] sm:bottom-4 p-2 md:p-5 flex items-center justify-between gap-4 mx-auto w-[80%] backdrop-blur-[30px]  rounded-sm text-black overflow-hidden"
     >
       <div>
-        <h4 className="text-white font-bold mb-1 text-[19px]">Portfolio</h4>
-        <span className="text-[#d9d9d9] text-sm">Branded Ecommerce</span>
+        <h4 className="text-white font-bold mb-1 text-sm xl:text-[19px]">Portfolio</h4>
+        <span className="text-[#d9d9d9] text-xs xl:text-sm">Branded Ecommerce</span>
       </div>
-      <div className="border-l border-solid border-white/60 pl-8">
-        <Link href={"/"} className="w-[45px] h-[45px] flex items-center justify-center rounded-[100%] transition-all duration-150 hover:bg-blue-500 bg-white/20">
-          <FaArrowRightLong size={23} color="white" />
+      <div className="border-l border-solid border-white/60 pl-2 md:pl-8">
+        <Link href={"/"} className="w-[30px] h-[30px] xl:w-[45px] xl:h-[45px] flex items-center justify-center rounded-[100%] transition-all duration-150 hover:bg-blue-500 bg-white/20">
+          <FaArrowRightLong size={18} color="white" />
         </Link>
       </div>
     </motion.div>
