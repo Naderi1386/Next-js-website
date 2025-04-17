c04a455de906291c26dfc5c94c66230cfa89c89b
unknown <mh.naderi1386@gmail.com>
2025-04-17 13:41:22 +0330

    change ui

--- app/_components/Brands.tsx
+++ app/_components/Brands.tsx
@@ -1,19 +1,44 @@
 import Image from "next/image";
 
 const brands = [
-  "https://go.demo.nextjstemplates.com/images/brands/uideck.svg",
-  "https://go.demo.nextjstemplates.com/images/brands/tailadmin.svg",
-  "https://go.demo.nextjstemplates.com/images/brands/graygrids.svg",
-  "https://go.demo.nextjstemplates.com/images/brands/lineicons.svg",
-  "https://go.demo.nextjstemplates.com/images/brands/plainadmin.svg",
+  {
+    img: "https://go.demo.nextjstemplates.com/images/brands/uideck.svg",
+    href: "https://uideck.com/",
+  },
+  {
+    img: "https://go.demo.nextjstemplates.com/images/brands/tailadmin.svg",
+    href: "https://tailadmin.com/",
+  },
+  {
+    img: "https://go.demo.nextjstemplates.com/images/brands/graygrids.svg",
+    href: "https://graygrids.com/",
+  },
+  {
+    img: "https://go.demo.nextjstemplates.com/images/brands/lineicons.svg",
+    href: "https://lineicons.com/",
+  },
+  {
+    img: "https://go.demo.nextjstemplates.com/images/brands/plainadmin.svg",
+    href: "https://plainadmin.com/",
+  },
 ];
 const Brands = () => {
-  return <ul className="flex items-center justify-center gap-8">
-    {brands.map((brand,index)=><li className="h-[40px] w-[175px] relative" key={index}>
-        <Image fill className="object-center" src={brand} alt={`brand-${index}`} />
-    </li>)}
-
-  </ul>;
+  return (
+    <ul className="flex items-center justify-center gap-8">
+      {brands.map((brand, index) => (
+        <li key={index}>
+          <a className="block h-[40px] w-[175px] relative" href={brand.href}>
+            <Image
+              fill
+              className="object-center"
+              src={brand.img}
+              alt={`brand-${index}`}
+            />
+          </a>
+        </li>
+      ))}
+    </ul>
+  );
 };
 
 export default Brands;
--- app/_components/FinallSection.tsx
+++ app/_components/FinallSection.tsx
@@ -4,7 +4,7 @@ import Brands from "./Brands";
 const FinallSection = () => {
   return (
     <section>
-      <div className="px-8 sm:px-18 py-40 bg-[#0c0a09]">
+      <div className="px-8 sm:px-18 pt-40 pb-20 bg-[#0c0a09]">
         <div className="text-center text-white relative">
           <h2 className="font-bold md:w-[582px] md:mx-auto text-[34px]  md:text-[45px] leading-[58px]">
             Trusted by Global <br className="hidden md:block" /> Brands
