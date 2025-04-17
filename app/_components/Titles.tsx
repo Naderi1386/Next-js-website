a07bf2f2196c62e3f58092a8886d04aabf1847e6
unknown <mh.naderi1386@gmail.com>
2025-04-17 15:34:05 +0330

    add some codes

--- app/_components/Titles.tsx
+++ app/_components/Titles.tsx
@@ -1,8 +1,31 @@
-
-const Titles = () => {
-  return (
-    <div>Titles</div>
-  )
+import { ReactNode } from "react";
+import Title from "./Title";
+interface TitlesPropsType {
+  titleText?: string;
+  mainTitle: string;
+  caption: string;
+  children?: ReactNode;
 }
 
-export default Titles
+const Titles = ({
+  titleText,
+  mainTitle,
+  caption,
+  children,
+}: TitlesPropsType) => {
+  return (
+    <div className="px-8 sm:px-18 py-40 bg-[#0c0a09]">
+      <div className="text-center text-white relative">
+        {titleText && <Title>{titleText}</Title>}
+
+        <h2 className="font-bold md:w-[582px] md:mx-auto text-[34px]  md:text-[45px] leading-[58px]">
+          {mainTitle}
+        </h2>
+        <p className="mt-7 md:mx-auto md:w-[640px] text-[#79808a]">{caption}</p>
+      </div>
+      {children && children}
+    </div>
+  );
+};
+
+export default Titles;
